74d2e6026358a9ed71541b6f44fec0a5f4f65e35
EdgarJP006 <p.j_006@hotmail.com>
2021-11-15 16:42:32 -0500

    Practicas TemplateStrings

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,3 +1,18 @@
 
 
-console.log('???');
+const nombre = 'Edgar';
+
+const apellido = 'Pérez';
+
+//Forma antigua de concatenar
+
+// const nombreCompleto = nombre + ' '+ apellido;
+
+//Nueva forma de concatenar con TemplateStrings
+
+const nombreCompleto =  `
+${nombre} 
+${apellido}
+${ 1 +1 }`;
+
+console.log( nombreCompleto);
